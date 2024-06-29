0789b374675a92e6747306d565abeb6e751d985a
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-06-29 04:12:37 -1000

    Add function to pass first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    for (let i = 0 ; i < num ; i++) {
+        if (i < num) {
+            finalString += `${string}`;
+        }
+    }
+    return finalString;
 };
 
 // Do not edit below this line
