312f635cc1b8df461a44ba137dd4cc582a0ebc0e
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-06-29 04:27:41 -1000

    Fix loop to return error for negative nums

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,12 @@
 const repeatString = function(string, num) {
     let finalString = "";
-    for (let i = 0 ; i < num ; i++) {
-        if (i < num) {
-            finalString += `${string}`;
+    if (num < 0) {
+        finalString = "ERROR";
+    } else {
+        for (let i = 0 ; i < num ; i++) {
+            if (i < num) {
+                finalString += `${string}`;
+            }
         }
     }
     return finalString;
