ebee8f5ffd72fe4bdc310ed0b8f52a4824e8a6f2
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-06-30 02:06:20 -1000

    Create function that passes first test
    Returns all number between and including 2 numbers together

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(value1, value2) {
+    let finalVal = 0;
+    for (i = value1; i <=value2; i++) {
+        finalVal += i;
+    }
+    return finalVal;
 };
 
 // Do not edit below this line
