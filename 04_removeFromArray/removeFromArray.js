28a8cc5ec18345eda498f7786659ad512649eceb
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-06-29 15:33:58 -1000

    Create function to remove item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(anArray, toRem) {
+    for (let i = 0; i < anArray.length; i++) {
+        if (anArray[i] == toRem) {
+            anArray.splice(i, 1);
+        }
+        else {
+            continue;
+        }
+    }
+    return anArray;
 };
 
 // Do not edit below this line
