25426cd6de198e3b7f51b39270b515719e2354cf
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-16 02:01:56 -1000

    Create function to pass first test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibArray = [1, 1];
+    for (let i = 1; i < num-1; i++) {
+        let nextNum = (fibArray[i-1] + fibArray[i]);
+        fibArray.push(nextNum);
+    }
+    return fibArray[num-1];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+/* fibonacci
+notes
+    -sequence used: 1, 1, 2, 3, 5, 8, etc.
+    -take an input number
+    -run fibonacci sequence until input number is reached
+    -return the number that the sequence ends up at
+pseudocode
+    -initiate function with one parameter
+        -parameter is a number that the sequence will run to
+    -create an array with numbers 1 and 1 to initialize the sequence
+*/
