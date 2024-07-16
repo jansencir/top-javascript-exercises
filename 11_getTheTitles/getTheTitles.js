60bd0c3b2807410194b1a5fca6608fe6fa697038
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-16 02:19:58 -1000

    Write function to return titles
    Passed all test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map(book => book.title)
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+/* pseudocode
+    -use map to search through objects and pick out titles only
+*/
