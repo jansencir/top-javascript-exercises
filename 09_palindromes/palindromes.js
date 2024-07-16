8a557256bc6de4a4ae09bd15de70c6f242975e16
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-15 20:52:11 -1000

    Write function to compare reversed string
    Passed first test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    return string == string
+        .split()
+        .reverse()
+        .join("");
 };
+
+/* pseudocode
+    -
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
