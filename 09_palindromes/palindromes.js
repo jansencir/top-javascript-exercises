476ae5a84812079f7ed342a2c6a2422bef8c00fb
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-15 21:01:59 -1000

    Restart function
    Only returned true, need to rework

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,17 @@
 const palindromes = function (string) {
-    return string == string
-        .split()
-        .reverse()
-        .join("");
+    let reverseString = "";
+    for (let i = string.length -1; i >= 0; i--) {
+        reverseString += string[i];
+    }    
+    return string == reverseString;
 };
 
 /* pseudocode
-    -
+    -take a string as a parameter
+    -compare the string to the reversed string
+        -turn it into an array, split
+        -reverse the array, reverse
+        -join it back together, join
 */
 
 // Do not edit below this line
